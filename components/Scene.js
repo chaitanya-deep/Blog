3822036ffb291f2f01803d2ff15a92905c5be74d
Chaitanya Deep <deep@euclidxr.com>
2022-01-19 07:46:54 +0530

    Initialize single canvas in Back component

--- components/Scene.js
+++ components/Scene.js
@@ -29,32 +29,18 @@ const Back = () => {
 
     const { theme, setTheme, resolvedTheme } = useTheme('dark')
     const terrain = useRef()
-    return( 
-        (theme=='dark')? (
-            <Canvas>
-            <directionalLight intensity={1} />
-            <ambientLight intensity={0.1}/>
-            <mesh
-              visible position={[0, -30 , 0]} rotation={[-Math.PI / 2, 0, 0]} ref={terrain} >
-              <planeBufferGeometry attach="geometry" args={[5000, 5000, 128, 128]} />
-                 <meshStandardMaterial  attach="material" 
-                       color= "teal" roughness={1} metalness={0} wireframe />
-            </mesh>
-           </Canvas>
-        ): (
-            <Canvas>
-            <directionalLight intensity={1} />
-            <ambientLight intensity={0.1}/>
-            <mesh
-              visible position={[0, -30 , 0]} rotation={[-Math.PI / 2, 0, 0]} ref={terrain} >
-              <planeBufferGeometry attach="geometry" args={[5000, 5000, 128, 128]} />
-                 <meshStandardMaterial  attach="material" 
-                       color= "pink" roughness={1} metalness={0} wireframe />
-            </mesh>
-           </Canvas>
-        )
-       
-    );
+  return (
+    <Canvas>
+      <directionalLight intensity={1} />
+      <ambientLight intensity={0.1}/>
+      <mesh
+        visible position={[0, -30 , 0]} rotation={[-Math.PI / 2, 0, 0]} ref={terrain} >
+        <planeBufferGeometry attach="geometry" args={[5000, 5000, 128, 128]} />
+        <meshStandardMaterial  attach="material"
+                               color={(theme=='dark') ? "teal" : "pink"} roughness={1} metalness={0} wireframe />
+      </mesh>
+    </Canvas>
+  )
 }
 
 
